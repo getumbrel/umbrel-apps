5210fdb4cedd58c52b641f5527614a799c6b4c30
Steven Briscoe <stevebriscoe123@gmail.com>
2022-05-30 01:35:30 +0100

    Fix typo

--- elements/exports.sh
+++ elements/exports.sh
@@ -1,8 +1,8 @@
-exports APP_ELEMENTS_PORT="3042"
-exports APP_ELEMENTS_IP="10.21.21.91"
-exports APP_ELEMENTS_NODE_IP="10.21.21.92"
-exports APP_ELEMENTS_NODE_RPC_PORT="7041"
-exports APP_ELEMENTS_NODE_P2P_PORT="18332"
+export APP_ELEMENTS_PORT="3042"
+export APP_ELEMENTS_IP="10.21.21.91"
+export APP_ELEMENTS_NODE_IP="10.21.21.92"
+export APP_ELEMENTS_NODE_RPC_PORT="7041"
+export APP_ELEMENTS_NODE_P2P_PORT="18332"
 
 local app_elements_rpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
 local app_elements_p2p_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-p2p/hostname"
