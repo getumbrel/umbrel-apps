3f2a42bc22394eab3b2c89ac0cb7d867766af68d
Luke Childs <lukechilds123@gmail.com>
2022-06-07 11:16:11 +0700

    env vars

--- core-lightning/exports.sh
+++ core-lightning/exports.sh
@@ -4,5 +4,9 @@ export APP_CORE_LIGHTNING_REST_IP="10.21.21.95"
 export APP_CORE_LIGHTNING_REST_PORT="2104"
 export APP_CORE_LIGHTNING_DAEMON_IP="10.21.21.96"
 export APP_CORE_LIGHTNING_DAEMON_PORT="9736"
+export APP_CORE_LIGHTNING_DAEMON_GRPC_PORT="9736"
 
 export APP_CORE_LIGHTNING_REST_CERT_DIR="${EXPORTS_APP_DIR}/data/c-lightning-rest/certs"
+
+rest_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
+export APP_CORE_LIGHTNING_REST_HIDDEN_SERVICE="$(cat "${rest_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
