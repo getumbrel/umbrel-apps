1c9803d5350d35ef76d792108044878c29b96ea0
Luke Childs <lukechilds123@gmail.com>
2022-06-07 18:10:58 +0700

    Add Core Lightning and RTL for CLN (#7)

---
+++ core-lightning/exports.sh
@@ -0,0 +1,12 @@
+export APP_CORE_LIGHTNING_IP="10.21.21.94"
+export APP_CORE_LIGHTNING_PORT="2103"
+export APP_CORE_LIGHTNING_REST_IP="10.21.21.95"
+export APP_CORE_LIGHTNING_REST_PORT="2104"
+export APP_CORE_LIGHTNING_DAEMON_IP="10.21.21.96"
+export APP_CORE_LIGHTNING_DAEMON_PORT="9736"
+export APP_CORE_LIGHTNING_DAEMON_GRPC_PORT="2105"
+
+export APP_CORE_LIGHTNING_REST_CERT_DIR="${EXPORTS_APP_DIR}/data/c-lightning-rest/certs"
+
+rest_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
+export APP_CORE_LIGHTNING_REST_HIDDEN_SERVICE="$(cat "${rest_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
