ffa76bc0623f55b87291435a3a8e91b7bb97e33e
Luke Childs <lukechilds123@gmail.com>
2022-06-08 13:58:31 +0700

    Patch app script to not wait for Tailscale

--- tailscale/exports.sh
+++ tailscale/exports.sh
@@ -1,2 +1,6 @@
 export APP_TAILSCALE_IP="10.21.21.80"
 export APP_TAILSCALE_PORT="8240"
+
+echo "Manually patching app script"
+# We need to do this because waiting for Tor HS fails due to being unable to resolve the (disabled for Tailscale) app proxy
+sed -i 's/^  wait_for_tor_hs/  [[ "${app}" != "tailscale"  ]] \&\& wait_for_tor_hs/g' "${UMBREL_ROOT}/scripts/app"
