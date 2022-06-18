413c3769ec40ee408e86bd8da39eb1ddcf056737
Jonathan Zernik <yzernik@gmail.com>
2022-06-17 17:39:00 -0700

    Fix exported core-lightning rpc file path (#7)

--- core-lightning/exports.sh
+++ core-lightning/exports.sh
@@ -7,7 +7,7 @@ export APP_CORE_LIGHTNING_DAEMON_PORT="9736"
 export APP_CORE_LIGHTNING_DAEMON_GRPC_PORT="2105"
 
 export APP_CORE_LIGHTNING_REST_CERT_DIR="${EXPORTS_APP_DIR}/data/c-lightning-rest/certs"
-export APP_CORE_LIGHTNING_RPC_FILE_PATH="${EXPORTS_APP_DIR}/data/.lightning/bitcoin/lightning-rpc"
+export APP_CORE_LIGHTNING_RPC_FILE_PATH="${EXPORTS_APP_DIR}/data/lightningd/bitcoin/lightning-rpc"
 
 rest_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
 export APP_CORE_LIGHTNING_REST_HIDDEN_SERVICE="$(cat "${rest_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
