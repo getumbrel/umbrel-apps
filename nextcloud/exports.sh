b431720c27ec8d8a514cb7445c1fe1d8025f861c
Luke Childs <lukechilds123@gmail.com>
2022-09-14 16:35:15 +0700

    Fix Nextcloud setup failure over IP

--- nextcloud/exports.sh
+++ nextcloud/exports.sh
@@ -3,3 +3,6 @@ export APP_NEXTCLOUD_IP="10.21.21.32"
 export APP_NEXTCLOUD_DB_IP="10.21.21.33"
 export APP_NEXTCLOUD_REDIS_IP="10.21.21.34"
 export APP_NEXTCLOUD_CRON_IP="10.21.21.35"
+
+local_ips=$(hostname --all-ip-addresses 2> /dev/null) || local_ips=""
+export APP_NEXTCLOUD_TRUSTED_DOMAINS="${APP_DOMAIN}:${APP_NEXTCLOUD_PORT} ${APP_HIDDEN_SERVICE} ${DEVICE_HOSTNAME}:${APP_NEXTCLOUD_PORT} ${local_ips}"
