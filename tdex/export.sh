72c8626f9bb308209435b7be259f96ada5e7b307
Steven Briscoe <me@stevenbriscoe.com>
2022-10-07 12:25:39 +0100

    Add exports.sh to expose daemon Tor HS

---
+++ tdex/export.sh
@@ -0,0 +1,2 @@
+daemon_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-daemon/hostname"
+export APP_TDEX_TRADER_HIDDEN_SERVICE="$(cat "${daemon_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
