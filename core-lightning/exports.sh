8feaf2283a1ef80d4cd886e576b9c55652063db9
tanakei <100189072+GDur4n66@users.noreply.github.com>
2022-10-31 12:22:40 +0900

    support testnet,signet,regtest for core-lightning

--- core-lightning/exports.sh
+++ core-lightning/exports.sh
@@ -8,5 +8,19 @@ export APP_CORE_LIGHTNING_DAEMON_GRPC_PORT="2105"
 
 export APP_CORE_LIGHTNING_REST_CERT_DIR="${EXPORTS_APP_DIR}/data/c-lightning-rest/certs"
 
+if [[ "${BITCOIN_NETWORK}" == "mainnet" ]]; then
+	export APP_CORE_LIGHTNING_BITCOIN_CHAIN="bitcoin"
+	export APP_CORE_LIGHTNING_DAEMON_PORT="9735"
+elif [[ "${BITCOIN_NETWORK}" == "testnet" ]]; then
+	export APP_CORE_LIGHTNING_BITCOIN_CHAIN="testnet"
+	export APP_CORE_LIGHTNING_DAEMON_PORT="19735"
+elif [[ "${BITCOIN_NETWORK}" == "signet" ]]; then
+	export APP_CORE_LIGHTNING_BITCOIN_CHAIN="signet"
+	export APP_CORE_LIGHTNING_DAEMON_PORT="39735"
+elif [[ "${BITCOIN_NETWORK}" == "regtest" ]]; then
+	export APP_CORE_LIGHTNING_BITCOIN_CHAIN="regtest"
+	export APP_CORE_LIGHTNING_DAEMON_PORT="19846"
+fi
+
 rest_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
 export APP_CORE_LIGHTNING_REST_HIDDEN_SERVICE="$(cat "${rest_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
