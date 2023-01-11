ac8f091d9b2b11f8ba7af77b94c8705e8e5bf90f
Steven Briscoe <me@stevenbriscoe.com>
2023-01-11 13:42:35 +0000

    Change db credentials

--- immich/exports.sh
+++ immich/exports.sh
@@ -1,5 +1,5 @@
-export APP_IMMICH_DB_USERNAME="postgres"
-export APP_IMMICH_DB_PASSWORD="postgres"
+export APP_IMMICH_DB_USERNAME="immich"
+export APP_IMMICH_DB_PASSWORD="moneyprintergobrrr"
 export APP_IMMICH_DB_DATABASE_NAME="immich"
 export APP_IMMICH_LOG_LEVEL="simple"
 export APP_IMMICH_DISABLE_REVERSE_GEOCODING="false"
