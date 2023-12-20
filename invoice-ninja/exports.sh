608d4534f53c6bf143b2c90cc09c447f0b058149
nmfretz <nmfretz@gmail.com>
2023-12-20 14:41:48 -0800

    Add APP_KEY generation

--- invoice-ninja/exports.sh
+++ invoice-ninja/exports.sh
@@ -1,2 +1,5 @@
-# Generate a new APP_KEY
-export NEW_APP_KEY=$(docker run --rm -it invoiceninja/invoiceninja php artisan key:generate --show)
+LARAVEL_APP_KEY_FILE_PATH="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))/data/laravel-app-key.txt"
+
+if [[ -f "${LARAVEL_APP_KEY_FILE_PATH}" ]]; then
+  export APP_KEY=$(cat "${LARAVEL_APP_KEY_FILE_PATH}")
+fi
