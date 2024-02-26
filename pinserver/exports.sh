3456f40c07fe68bc78af4159f8967b0724c3c8e2
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-02-26 08:37:13 +0100

    align to version 0.1.0

--- pinserver/exports.sh
+++ pinserver/exports.sh
@@ -1,7 +1,8 @@
-export APP_PINSERVER_PORT="8096"
+export APP_PINSERVER_PORT="8097"
 export APP_PINSERVER_WEB_PORT="8095"
-local app_pinserver_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}/hostname"
 
-export APP_PINSERVER_HIDDEN_SERVICE="$(cat "${app_pinserver_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
+local app_pinserver_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-node/hostname"
 
-export APP_TAILSCALE_URL="$(hostname 2>/dev/null || echo "notyetset.tailscale")"
+export APP_PINSERVER_HIDDEN_SERVICE="http://$(cat "${app_pinserver_hidden_service_file}" 2>/dev/null || echo "http://notyetset.onion")"
+
+export APP_TAILSCALE_URL="http://$(hostname 2>/dev/null || echo "http://notyetset.tailscale")"
