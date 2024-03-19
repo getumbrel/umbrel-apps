6e0e6e7bf4b09ef4ece7149e7ae2ede803206345
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-03-19 21:22:06 +0100

    release 0.1.1 - pinserver update for the new jade's firmware

--- pinserver/exports.sh
+++
@@ -1,8 +0,0 @@
-export APP_PINSERVER_PORT="8097"
-export APP_PINSERVER_WEB_PORT="8095"
-
-local app_pinserver_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-node/hostname"
-
-export APP_PINSERVER_HIDDEN_SERVICE="http://$(cat "${app_pinserver_hidden_service_file}" 2>/dev/null || echo "http://notyetset.onion")"
-
-export APP_TAILSCALE_URL="http://$(hostname 2>/dev/null || echo "http://notyetset.tailscale")"
