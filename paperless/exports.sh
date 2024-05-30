9acb0411e59587ed4664028a00fb88630304054a
nmfretz <nmfretz@gmail.com>
2024-05-30 12:41:05 +1000

    tweak exports.sh

--- paperless/exports.sh
+++ paperless/exports.sh
@@ -1,13 +1,13 @@
-# make sure that UMBREL_DATA_DIR/storage/paperless/import and UMBREL_DATA_DIR/storage/paperless/export exist and are owned by 1000:1000
+# Ensure that Paperless consume and export directories are created with the correct permissions
 UMBREL_DATA_DIR="${UMBREL_ROOT}/data"
 UMBREL_DATA_STORAGE_DIR="${UMBREL_DATA_DIR}/storage"
 UMBREL_DATA_STORAGE_PAPERLESS_DIR="${UMBREL_DATA_STORAGE_DIR}/paperless"
-UMBREL_DATA_STORAGE_PAPERLESS_IMPORT_DIR="${UMBREL_DATA_STORAGE_PAPERLESS_DIR}/import"
+UMBREL_DATA_STORAGE_PAPERLESS_CONSUME_DIR="${UMBREL_DATA_STORAGE_PAPERLESS_DIR}/consume"
 UMBREL_DATA_STORAGE_PAPERLESS_EXPORT_DIR="${UMBREL_DATA_STORAGE_PAPERLESS_DIR}/export"
 DESIRED_OWNER="1000:1000"
 
-if [[ ! -d "${UMBREL_DATA_STORAGE_PAPERLESS_IMPORT_DIR}" ]]; then
-  mkdir -p "${UMBREL_DATA_STORAGE_PAPERLESS_IMPORT_DIR}"
+if [[ ! -d "${UMBREL_DATA_STORAGE_PAPERLESS_CONSUME_DIR}" ]]; then
+  mkdir -p "${UMBREL_DATA_STORAGE_PAPERLESS_CONSUME_DIR}"
 fi
 
 if [[ ! -d "${UMBREL_DATA_STORAGE_PAPERLESS_EXPORT_DIR}" ]]; then
@@ -28,5 +28,5 @@ set_paperless_correct_permissions() {
 
 set_paperless_correct_permissions "${UMBREL_DATA_STORAGE_DIR}"
 set_paperless_correct_permissions "${UMBREL_DATA_STORAGE_PAPERLESS_DIR}"
-set_paperless_correct_permissions "${UMBREL_DATA_STORAGE_PAPERLESS_IMPORT_DIR}"
+set_paperless_correct_permissions "${UMBREL_DATA_STORAGE_PAPERLESS_CONSUME_DIR}"
 set_paperless_correct_permissions "${UMBREL_DATA_STORAGE_PAPERLESS_EXPORT_DIR}"
