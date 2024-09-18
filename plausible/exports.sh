0456dd1b2603ad5d98bb22ba6bb055a31c10521e
nmfretz <nmfretz@gmail.com>
2024-09-18 14:15:26 +1000

    Deterministically derive vault and secret key

--- plausible/exports.sh
+++ plausible/exports.sh
@@ -1,2 +1,2 @@
-export APP_PLAUSIBLE_SECRET_KEY="$(openssl rand -base64 48)"
-export APP_PLAUSIBLE_VAULT_KEY="$(openssl rand -base64 32)"
+# 32 byte long string used for encrypting TOTP secrets at rest using AES256-GCM. Must not change on app restart.
+export APP_PLAUSIBLE_VAULT_KEY=$(openssl dgst -sha256 -binary <<< "$(derive_entropy "env-${app_entropy_identifier}-APP_VAULT_KEY")" | base64)
