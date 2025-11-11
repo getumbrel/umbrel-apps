d050fe5499991b1728053ca71f63cc9400f0a4ed
nmfretz <nmfretz@gmail.com>
2025-11-11 10:31:57 +1000

    Rename lightning hidden service env vars

--- core-lightning/exports.sh
+++ core-lightning/exports.sh
@@ -12,8 +12,8 @@ if [[ "${APP_BITCOIN_NETWORK}" == "mainnet" ]]; then
 	export APP_CORE_LIGHTNING_BITCOIN_NETWORK="bitcoin"
 fi
 
-rest_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
-export APP_CORE_LIGHTNING_REST_HIDDEN_SERVICE="$(cat "${rest_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
+lightning_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
+export APP_CORE_LIGHTNING_HIDDEN_SERVICE="$(cat "${lightning_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
 
 export APP_CONFIG_DIR="/data/app"
 export APP_MODE="production"
