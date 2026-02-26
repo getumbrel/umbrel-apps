f96507dc67ff4392b49480ac6895592d21ba5631
SatWise <edjanx@gmail.com>
2026-02-26 15:42:11 -0500

    Add exports paths for to release DR build back to umbrelOS harness
    exports required to bypass legacy app start-up commands in /hooks/pre-start

--- core-lightning/exports.sh
+++ core-lightning/exports.sh
@@ -1,3 +1,8 @@
 #DR ABSTRACTS CLN_REST_BITCOIN_NETWORK="${APP_BITCOIN_NETWORK}" OS build HARD-CODE
 export APP_CLN_REST_BITCOIN_NETWORK="bitcoin"
 export APP_CORE_LIGHTNING_REST_MACAROON_PATH="${APP_DATA_DIR}/data/lightningd/bitcoin/rest/access.macaroon"
+# --- Custom Volume Variables for 4-Tier Architecture ---
+export APP_CONFIG_DIR="${APP_DATA_DIR}/data/config"
+export APP_CLN_DATA_DIR="${APP_DATA_DIR}/data/lightningd"
+export APP_CLN_REST_DATA_DIR="${APP_DATA_DIR}/data/clnrest"
+export CLN_REST_PATH="/tmp/clnrest"
