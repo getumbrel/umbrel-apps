8f0f3fa55ce49bf908ae70a6e65dc89c02bd2ecb
SatWise <edjanx@gmail.com>
2026-03-01 22:59:07 -0500

    Add exports.sh for CLN resource discovery

---
+++ umbrel-lnbits-cln/exports.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Environment variable exports for CLN resource discovery
+
+export CLN_RESOURCE_DISCOVERY_URL="https://example.com/cln_discovery"
+export CLN_API_KEY="your_api_key_here"
+export CLN_TIMEOUT="30"
