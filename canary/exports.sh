c6686fc8181876407feddb6519e592eb174c9d68
Andreas Schjønhaug <andreas@schjonhaug.com>
2026-05-12 15:20:13 +0200

    canary: Detect exact Mempool app id

--- canary/exports.sh
+++ canary/exports.sh
@@ -5,6 +5,6 @@ export APP_CANARY_PORT="3005"
 # Note: APP_MEMPOOL_PORT comes from Mempool's exports.sh, which is only sourced
 # if Mempool is a dependency. Since it's optional, we hardcode the known port.
 installed_apps=$("${UMBREL_ROOT}/scripts/app" ls-installed)
-if echo "$installed_apps" | grep --quiet 'mempool'; then
+if echo "$installed_apps" | grep --quiet '^mempool$'; then
   export APP_CANARY_MEMPOOL_PORT="3006"
 fi
