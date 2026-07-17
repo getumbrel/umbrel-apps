1d0c942ace82a1b4e0009bc10d40724356bb4a16
nmfretz <nmfretz@gmail.com>
2026-07-17 14:32:47 +1000

    Update LaWallet NWC to 1.4.0

--- lawallet-nwc/exports.sh
+++ lawallet-nwc/exports.sh
@@ -1,2 +1,3 @@
 export APP_LAWALLET_NWC_POSTGRES_PASSWORD="$(derive_entropy "${app_entropy_identifier}-postgres-password")"
 export APP_LAWALLET_NWC_JWT_SECRET="$(derive_entropy "${app_entropy_identifier}-jwt-secret")"
+export APP_LAWALLET_NWC_LISTENER_AUTH_SECRET="$(derive_entropy "${app_entropy_identifier}-listener-auth-secret")"
