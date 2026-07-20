af10a2669043a5ebd83aa9f46e0281d90d7f079d
nmfretz <nmfretz@gmail.com>
2026-07-20 13:11:51 +1000

    Use a stable node IP for companion apps

--- bitcoin-cash-node/exports.sh
+++ bitcoin-cash-node/exports.sh
@@ -1,8 +1,6 @@
 export APP_BITCOIN_CASH_NODE_TOR_IP="10.21.22.50"
+export APP_BITCOIN_CASH_NODE_NODE_IP="10.21.21.50"
 
-# Connection details for companion apps (Fulcrum BCH, SoloStrike Cash) to consume,
-# so they don't hardcode the container name, ports, or RPC credentials.
-export APP_BITCOIN_CASH_NODE_RPC_HOST="bitcoin-cash-node_bitcoind_1"
 export APP_BITCOIN_CASH_NODE_RPC_PORT="8332"
 export APP_BITCOIN_CASH_NODE_P2P_PORT="8335"
 export APP_BITCOIN_CASH_NODE_ZMQ_HASHBLOCK_PORT="28332"
